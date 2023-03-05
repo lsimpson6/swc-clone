969e07f257067d19950d7f0af3a2d1df0ac61d57
Logan Simpson <lsimpson6@liberty.edu>
2023-03-05 12:35:33 -0500

    Commenet out unused code tmp

--- swc.shewellness/Controllers/ErrorController.cs
+++ swc.shewellness/Controllers/ErrorController.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
 
-namespace swc.shewellness.Controllers
-{
-    public class ErrorController
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
+//namespace swc.shewellness.Controllers
+//{
+//    public class ErrorController
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
