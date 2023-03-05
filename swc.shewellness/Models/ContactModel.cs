389e0848b43a3340fcef26a7e3a0ee4ec009655d
Logan Simpson <lsimpson6@liberty.edu>
2023-03-05 11:29:12 -0500

    general site updates

--- swc.shewellness/Models/ContactModel.cs
+++ swc.shewellness/Models/ContactModel.cs
@@ -5,7 +5,8 @@ namespace swc.shewellness.Models
 {
     public class ContactModel
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         [Required]
         [EmailAddress]
         public string Email { get; set; }
--- swc.shewellness/wwwroot/js/animation.js
+++ swc.shewellness/wwwroot/js/animation.js
@@ -4,8 +4,7 @@
 
   function init() {
       elements = document.querySelectorAll('.animate');
-      loaction = document.querySelectorAll('.locations');
-    windowHeight = window.innerHeight + 100;
+    windowHeight = window.innerHeight/2;
   }
 
   function checkPosition() {
@@ -13,9 +12,9 @@
       var element = elements[i];
       var positionFromTop = elements[i].getBoundingClientRect().top;
 
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('fade-in');
-          element.classList.remove('opacity-0');
+        if ((positionFromTop - windowHeight) <= 0) {
+          element.classList.add('fade-up');
+          element.classList.remove('animate');
         }
     }
   }
@@ -29,85 +28,29 @@
 
 var screenHeight = (window.innerHeight)/3;
 
-var welcomeHeight = $('#welcome').offset().top, $window = $(window);
-var chk1 = document.getElementById('location-1');
-
-var factFiction = $('#fact-vs-fiction').offset().top, $window = $(window);
-var chk2 = document.getElementById('location-2');
-
-var uniquePhys = $('#unique-physiology').offset().top, $window = $(window);
-var chk3 = document.getElementById('location-3');
-
-var process = $('#the-process').offset().top, $window = $(window);
-var chk4 = document.getElementById('location-4');
-
-var about = $('#about').offset().top, $window = $(window);
-var chk5 = document.getElementById('location-5');
-
-$window.scroll(function () {
+(function () {
 
     var navColor = document.querySelector('#navigation-color');
-    var minusWelcome = document.getElementById('welcome').clientHeight;
-    var navHeight = document.getElementById('navigation-header').clientHeight;
+    var navCTA = document.querySelector('#nav-cta-btn');
 
-        if ($window.scrollTop() > ((welcomeHeight + minusWelcome) - navHeight)) {
-            navColor.classList.add('bg-navy');
+
+    function addHeaderColor() {
+        var tmpLoc = window.location.pathname;
+        if ((tmpLoc == " " || tmpLoc == "/" || tmpLoc == "Home")) {
+            if (window.scrollY > 30) {
+                navColor.classList.add('bg-red');
+                navCTA.classList.remove('cta-button');
+                navCTA.classList.add('cta-button-white');
+            }
+            else {
+                navColor.classList.remove('bg-red');
+                navCTA.classList.add('cta-button');
+                navCTA.classList.remove('cta-button-white');
+            }
         }
-        else {
-            navColor.classList.remove('bg-navy');
-        }
+    }
 
-})
+    window.addEventListener('scroll', addHeaderColor);
 
-document.addEventListener('load', () => {
 
-    var bodyPadding = document.getElementById('body-padding-top');
-    var headerHeight = document.querySelector('#navigation-header').getBoundingClientRect();
-
-    bodyPadding = headerHeight + 'px';
-    alert(bodyPadding)
-})
-
-//    // welcome
-//    if ($window.scrollTop() >= (welcomeHeight - screenHeight)) {
-//        chk1.style.backgroundColor = '#8F6152';
-
-//    }
-//    else {
-//        chk1.style.backgroundColor = 'transparent';
-//    }
-
-//    // why she wellness
-//    if ($window.scrollTop() >= (factFiction - navHeight)) {
-//        chk2.style.backgroundColor = '#8F6152';
-//    }
-//    else {
-//        chk2.style.backgroundColor = 'transparent';
-//    }
-
-//    // about me - my story
-//    if ($window.scrollTop() >= (uniquePhys - navHeight)) {
-//        chk3.style.backgroundColor = '#8F6152';
-//    }
-//    else {
-//        chk3.style.backgroundColor = 'transparent';
-//    }
-
-//    // end
-//    if ($window.scrollTop() >= (process - navHeight)) {
-//        chk4.style.backgroundColor = '#8F6152';
-//    }
-//    else {
-//        chk4.style.backgroundColor = 'transparent';
-//    }
-
-//    // end
-//    if ($window.scrollTop() >= (about - navHeight - (minusWelcome/2))) {
-//        chk5.style.backgroundColor = '#8F6152';
-//    }
-//    else {
-//        chk5.style.backgroundColor = 'transparent';
-//    }
-
-    
-//})
+})();
