495af0f31faa3bb93f52fbcb4ed293b98b7b2edf
Logan Simpson <lsimpson6@liberty.edu>
2023-08-06 12:39:49 -0400

    added 404 handling

---
+++ swc.shewellness/Models/ErrorModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace swc.shewellness.Models
+{
+    public class ErrorModel
+    {
+        public int ErrorCode { get; set; }
+    }
+}
--- swc.shewellness/Program.cs
+++ swc.shewellness/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    //app.Use(async (context, next) =>
+    //{
+    //    await next();
+    //    if (context.Response.StatusCode == 404)
+    //    {
+    //        context.Request.Path = "/Home";
+    //        await next();
+    //    }
+    //});
 }
 
 app.UseHttpsRedirection();
@@ -21,6 +32,22 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+//app.Use(async (context, next) =>
+//{
+//    await next();
+//    getSetCode(context.Response.StatusCode);
+//    await next();
+//});
+
+//void getSetCode(int code)
+//{
+//    app.UseStatusCodePagesWithReExecute($"/Error/NotFound/{code}");
+//}
+
+//app.UseStatusCodePagesWithReExecute($"/Error/NotFound/{404}");
+
+app.UseStatusCodePagesWithReExecute($"/Error/NotFound");
 
 app.MapControllerRoute(
     name: "default",
@@ -37,5 +64,9 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name: "admin",
     pattern: "{controller=Admin}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "error",
+    pattern: "{controller=Error}/{action=Index}/{id?}");
 
 app.Run();
